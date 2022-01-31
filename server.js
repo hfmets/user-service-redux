fde621d8cdbdb3015c2d675a47157903226ef691
Hayden Farnsworth <hfmets@gmail.com>
2022-01-31 13:36:00 -0600

    change to session config

--- server.js
+++ server.js
@@ -16,23 +16,27 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(
-  session({
-    secret: "keyboard cat",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      sameSite: "none",
-      maxAge: 30 * 24 * 60 * 60 * 1000,
-      httpOnly: false,
-    },
-    store: new SequelizeStore({
-      db: sequelize,
-      table: "Sessions",
-      checkExpirationInterval: 15 * 60 * 1000,
-    }),
-  })
-);
+const sessionConfig = {
+  secret: "keyboard cat",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    sameSite: "none",
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    httpOnly: false,
+  },
+  store: new SequelizeStore({
+    db: sequelize,
+    table: "Sessions",
+    checkExpirationInterval: 15 * 60 * 1000,
+  }),
+};
+
+if (process.env.NODE_ENV === "production") {
+  sessionConfig.cookie.secure = "true";
+}
+
+app.use(session(sessionConfig));
 
 const userRouter = require("./routes/userRouter");
 app.use("/api/users", userRouter);
