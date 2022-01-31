bf1b1c5086c5977338787ea74f037f4c3ed70644
Hayden Farnsworth <hfmets@gmail.com>
2022-01-31 16:02:43 -0600

    change cookie domain

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const sessionConfig = {
   saveUninitialized: false,
   proxy: true,
   cookie: {
-    domain: "localhost",
+    domain: "http://localhost:4200",
     sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
     httpOnly: false,
