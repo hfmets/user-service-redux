d3c62ce683bb1fc95fa3809d7f882e57d670aa04
Hayden Farnsworth <hfmets@gmail.com>
2022-01-31 15:41:23 -0600

    change origin

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const sequelize = require("./models/index");
 const cors = require("cors");
 
 const corsOptions = {
-  origin: true,
+  origin: "http://localhost:4200",
   credentials: true,
 };
 app.use(cors(corsOptions));
